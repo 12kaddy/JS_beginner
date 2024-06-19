d0b4c274e8865205ad789af1bc9befa2212b5669
Karthikeya Mishra <86558772+12kaddy@users.noreply.github.com>
2024-06-19 14:50:39 +0000

    worked with stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,19 +1,26 @@
-const myFunction = function(){
-    console.log("Helloworld")
-}
+// const myFunction = function(){
+//     console.log("Helloworld")
+// }
 
-console.log(typeof myFunction)
+// console.log(typeof myFunction)
 
-const heros = ["shaktiman", "Krish", "chotabheem"];
-let myObj = {
-    name: "hitesh",
-    age:22,
-}
+// const heros = ["shaktiman", "Krish", "chotabheem"];
+// let myObj = {
+//     name: "hitesh",
+//     age:22,
+// }
 
-console.log(typeof myObj)
-console.log(typeof heros)
+// console.log(typeof myObj)
+// console.log(typeof heros)
 
-const id = 32425
-const anotherId =32534
+// const id = 32425
+// const anotherId =32534
 
 
+// Stack & Heap
+
+let a = 10;
+let b = a;  // b gets a copy of a's value
+a = 20;
+console.log(a); // 20
+console.log(b); // 10
