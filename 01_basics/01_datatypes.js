9553002a02cf411cd053437cb9ee129a3397f94d
Karthikeya Mishra <86558772+12kaddy@users.noreply.github.com>
2024-06-19 07:23:42 +0000

    Worked and understood JS datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,11 @@
+let name = "Karthik"
+let age = 18
+let isLoggedin  = false
+let sum;
+
+console.table([
+    { Variable: 'name', Type: typeof name },
+    { Variable: 'age', Type: typeof age },
+    { Variable: 'isLoggedin', Type: typeof isLoggedin },
+    { Variable: 'sum', Type: typeof sum }, typeof null, typeof undefined
+]);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 242352
+var accountPassword = "sggsdfhs"
+let accountMail = "karth@gmail.com"
+
+// accountId = 53463
+accountMail = "dfsdf@gmail.com"
+accountPassword = "3525345"
+console.log(accountPassword)
+
+
+console.table([accountId, accountMail, accountPassword])
