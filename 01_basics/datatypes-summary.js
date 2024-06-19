923a3f993e649717ecafb2ff7755835be0c5fbca
Karthikeya Mishra <86558772+12kaddy@users.noreply.github.com>
2024-06-19 12:50:21 +0000

    Summararized datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+const myFunction = function(){
+    console.log("Helloworld")
+}
+
+console.log(typeof myFunction)
+
+const heros = ["shaktiman", "Krish", "chotabheem"];
+let myObj = {
+    name: "hitesh",
+    age:22,
+}
+
+console.log(typeof myObj)
+console.log(typeof heros)
+
+const id = 32425
+const anotherId =32534
+
+
