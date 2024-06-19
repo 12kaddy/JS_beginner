e3ec12d6e668649e41c46fa3f088f06c475bcf89
Karthikeya Mishra <86558772+12kaddy@users.noreply.github.com>
2024-06-19 11:15:35 +0000

    Worked with comparisosn and COnversion in Javascript

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,24 @@
+// let score = null
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// let gameCounter = 100
+// ++gameCounter
+// console.log(gameCounter)
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+for (let i = 0; i < 5; ++i) {
+    console.log(i);
+}
+
